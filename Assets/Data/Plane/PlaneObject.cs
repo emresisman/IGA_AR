e4868f6bb96489cafdfb3b1e459f3b96c3becd1a
Emre ŞİŞMAN <emresisman53@gmail.com>
2022-12-21 16:59:54 +0300

    Update PlaneObject.cs

--- Assets/Data/Plane/PlaneObject.cs
+++ Assets/Data/Plane/PlaneObject.cs
@@ -6,28 +6,40 @@ namespace Data.Plane
     [Serializable]
     public class PlaneObject
     {
-        [SerializeField] private string callSign;
-        public string CallSign {  
-            get => this.callSign;
-            set => this.callSign = value;
+        [SerializeField] private string flight_iata;
+        public string Flight_Iata {  
+            get => this.flight_iata;
+            set => this.flight_iata = value;
         }
-
-        [SerializeField] private string fromAirport;
-        public string FromAirport {  
-            get => this.fromAirport;
-            set => this.fromAirport = value;
+        [SerializeField] private string dep_iata;
+        public string Dep_Iata {  
+            get => this.dep_iata;
+            set => this.dep_iata = value;
         }
-        
-        [SerializeField] private string toAirport;
-        public string ToAirport {  
-            get => this.toAirport;
-            set => this.toAirport = value;
+        [SerializeField] private string arr_iata;
+        public string Arr_Iata {  
+            get => this.arr_iata;
+            set => this.arr_iata = value;
         }        
-        
-        [SerializeField] private DateTime arriveTime;
-        public DateTime ArriveTime {  
-            get => this.arriveTime;
-            set => this.arriveTime = value;
+        [SerializeField] private string dep_time;
+        public string Dep_Time {  
+            get => this.dep_time;
+            set => this.dep_time = value;
+        }        
+        [SerializeField] private string arr_time;
+        public string Arr_Time {  
+            get => this.arr_time;
+            set => this.arr_time = value;
+        }        
+        [SerializeField] private string dep_actual;
+        public string Dep_Actual {  
+            get => this.dep_actual;
+            set => this.dep_actual = value;
+        }        
+        [SerializeField] private string arr_estimated;
+        public string Arr_Estimated {  
+            get => this.arr_estimated;
+            set => this.arr_estimated = value;
         }
     }
 }
