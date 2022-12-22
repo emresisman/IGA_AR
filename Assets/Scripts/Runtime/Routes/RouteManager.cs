bc61732de897a282e316c89011d531b8c17849b9
Emre ŞİŞMAN <emresisman53@gmail.com>
2022-12-22 11:58:16 +0300

    RouteManager created.

---
+++ Assets/Scripts/Runtime/Routes/RouteManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Runtime.Routes
+{
+    public class RouteManager : Singleton<RouteManager>
+    {
+        [SerializeField] private List<Route> landingRoutes = new List<Route>();
+        [SerializeField] private List<Route> takeOffRoutes = new List<Route>();
+
+        public Route GetAvailableLandingRoute()
+        {
+            var available = landingRoutes.Where(x => x.isUsing == false).ToList();
+            var index = Random.Range(0, available.Count());
+            return available[index];
+        }        
+        
+        public Route GetAvailableTakeOffRoute()
+        {
+            var available = takeOffRoutes.Where(x => x.isUsing == false).ToList();
+            var index = Random.Range(0, available.Count());
+            return available[index];
+        }
+    }
+}
