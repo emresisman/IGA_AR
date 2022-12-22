f418b8794882f23c5f4b9123321d6ecf2a1e783e
Emre ŞİŞMAN <emresisman53@gmail.com>
2022-12-22 14:00:09 +0300

    Plane landing route changed.

--- Assets/Scripts/Runtime/Planes/PlaneManager.cs
+++ Assets/Scripts/Runtime/Planes/PlaneManager.cs
@@ -10,13 +10,6 @@ namespace Runtime.Planes
 
         public void CreatePlane(FlightResponse planeResponse)
         {
-            Debug.Log(planeResponse.Flight_Iata 
-                      + "\n" + planeResponse.Dep_Iata
-                      + "\n" + planeResponse.Arr_Iata
-                      + "\n" + planeResponse.Alt
-                      + "\n" + planeResponse.Dep_Time
-                      + "\n" + planeResponse.Arr_Time);
-            
             var plane = Instantiate(planePrefab, transform.position, Quaternion.identity);
             if (plane.TryGetComponent<Plane>(out var component))
             {
